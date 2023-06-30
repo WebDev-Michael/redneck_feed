b907707b0f52c411f8211d24ba1c30745da1236a
Krygore <webdev.krygore@gmail.com>
2023-06-29 22:20:50 -0700

    updated App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Navbar from './components/Navbar';
 
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-import Home from './pages/index';
+import Home from './pages';
 import AboutUs from './pages/AboutUs';
 import AboutXcel from './pages/aboutXcel';
 import Contact from './pages/contact';
@@ -12,22 +12,16 @@ import Products from './pages/products';
 
 function App() {
   return (
-    <div className="App">
-      <div className='App-header'>
-        <header>
-          <img src='' alt=''/>
-          <h1>Redneck Feed</h1>
-          <h3>Grain delivered within 2 business days</h3>
-          <img src='' alt=''/>
-        </header>
-      </div>
-      <div className='Navbar'>
-        <Navbar />
-      </div>
-      <div className='App-body'>
-        
-      </div>
-    </div>
+      <Router>
+          <Navbar />
+          <Routes>
+              <Route exact path='/' element={<Home />} />
+              <Route path='/about' element={<Contact />} />
+              <Route path='/contact' element={<AboutUs />} />
+              <Route path='/blogs' element={<AboutXcel />} />
+              <Route path='/sign-up' element={<Products />} />
+          </Routes>
+      </Router>
   );
 }
 
