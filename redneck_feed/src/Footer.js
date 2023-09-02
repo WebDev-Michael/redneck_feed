6075ff935b6c6c17186275295e8f8f9616d17d9a
Kaleb-Rupe <57547999+Kaleb-Rupe@users.noreply.github.com>
2023-09-02 00:09:25 -0400

    wip

---
+++ redneck_feed/src/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Footer(props) {
+  return (
+    <>
+      <footer className="centered bg-darkgray border padding-1515 margin-1515">
+        <small>*Prices subject to change without notice</small>
+        <p>Copyright Redneck Feed&copy;</p>
+      </footer>
+    </>
+  );
+}
