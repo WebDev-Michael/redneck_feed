5893b2e7f7d918c2a45a656dbb098e3c91f5632a
Krygore <webdev.krygore@gmail.com>
2023-09-07 18:56:46 -0700

    rewrote code and compiled

--- src/Products/Rabbit.js
+++ src/Products/Rabbit.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import feedBag from "../images/feedBag.png";
 
 export default function Rabbit() {
-  const [image, setImage] = useState(feedBag);
+  const [image] = useState(feedBag);
 
   return (
     <>
