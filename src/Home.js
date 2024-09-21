10ae912f96c8ee72674ffda1dfa033725234f40c
Michael-Wilcox-Web-LA <webdev.krygore@gmail.com>
2024-09-21 16:44:44 -0700

    wip

--- src/Home.js
+++ src/Home.js
@@ -8,11 +8,11 @@ export default function Home() {
       <div className="banner-container">
       <img src={bannerImage} alt="Redneck Feeds Logo" className="banner" />
       </div>
-      <CategorySection />
+      
       <main>
         {/* <!-- About Section --> */}
 
-        <div className="home centered border padding-1515">
+        <div className="home centered padding-1515">
           <h1 className="header centered bold">About RedNeck Feeds</h1>
           <h3 className="home-text">
             Serving a 25 mile radius from the heart of Belfair. XCEL Grains delivered
@@ -21,10 +21,10 @@ export default function Home() {
             <br></br> 
             <a className="phone" href="tel:(253) 313-8107">(253) 313-8107 </a>
           </h3>
-          <h4>** Areas beyond 25 mile range are subject to a minimum bag amount to order that is based on distance. Pleae call for details. </h4>
+          <h4>** Areas beyond 25 mile range are subject to a minimum bag amount to order that is based on distance. Please call for details. </h4>
         </div>
       </main>
-      
+      <CategorySection />
     </>
   );
 }
